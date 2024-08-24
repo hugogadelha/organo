06875799ee8641942c334ee982a29d1a5c147311
hugogadelha <hugogadelha@gmail.com>
2024-08-24 16:54:53 -0300

    att 24-08

---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import Botao from '../Botão'
+import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import './Formulario.css'
+
+const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-end',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido =>', nome, cargo, imagem)
+    }
+
+    return (
+        <section className='formulario'>
+            <form onSubmit={aoSalvar}>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto obrigatorio={true}label="Nome" placeholder="Digite seu nome" valor={nome} aoAlterado={valor => setNome(valor)} />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" valor={cargo} aoAlterado={valor => setCargo(valor)} />
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" valor={imagem} aoAlterado={valor => setImagem(valor)}/>
+                <ListaSuspensa label="Time" itens={times}/>
+                <Botao>
+                    Criar Card
+                </Botao>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
